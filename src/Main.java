3985cfed64d982d360c8a29daa99a0c60f2f4979
bobbobdude <thomasrobertgardner1056@gmail.com>
2023-04-20 15:19:19 +0200

    Did the 1.7 reading and refresher

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        String stringToPrint = stringAPIPractice15.replaceAllAWithX("HELLO WORLD A LOVELY DAY");
-        System.out.println(stringToPrint);
+
+        List <ReadTextFile.Person> persons = ReadTextFile.loadPersons();
+
+        System.out.println("Total number of persons: " + persons.size());
+
+        for (int i = 0; i < persons.size(); i++){
+            System.out.println(persons.get(i).getFirstName());
+        }
+
     }
-}
+
+    }
---
+++ src/ReadTextFile.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReadTextFile {
+    public static List loadPersons() {
+
+        List list = new ArrayList<>();
+
+        File file = new File("src/testdata.csv");
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                list.add(new Person(scanner.nextLine()));
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println("Could not find the file to be loaded");
+        }
+
+        return list;
+    }
+
+    public static class Person {
+        private int id;
+        private String firstName;
+        private String lastName;
+        private int age;
+        private String street;
+        private String city;
+        private String state;
+        private int zip;
+
+        public Person(String csvLine) {
+            String[] data = csvLine.split(",");
+            this.id = Integer.valueOf(data[0]);
+            this.firstName = data[1];
+            this.lastName = data[2];
+            this.age = Integer.valueOf(data[3]);
+            this.street = data[4];
+            this.city = data[5];
+            this.state = data[6];
+            this.zip = Integer.valueOf(data[7]);
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getFullName() {
+            return firstName + " " + lastName;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+}
