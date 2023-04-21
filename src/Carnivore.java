4e8dddac6e72ffe007b7b55101b58d3e3ae8e9d6
bobbobdude <thomasrobertgardner1056@gmail.com>
2023-04-21 15:03:51 +0200

    Doing Multilevel inheritance

---
+++ src/Carnivore.java
@@ -0,0 +1,34 @@
+public class Carnivore {
+    final private String diet = "Meat";
+
+    public String getDiet(){
+        return diet;
+    }
+}
+
+class Dinosaur extends Carnivore{
+    private String name;
+    private double height;
+    private int weight;
+
+    public Dinosaur(String name, double height, int weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+}
+
+class Tyrannosaurus extends Dinosaur{
+    public Tyrannosaurus(String name, double height, int weight){
+        super(name, height, weight);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List <ReadTextFile.Person> persons = ReadTextFile.loadPersons();
+        Tyrannosaurus tiny = new Tyrannosaurus("Jerry", 8.6, 50);
 
-        System.out.println("Total number of persons: " + persons.size());
-
-        for (int i = 0; i < persons.size(); i++){
-            System.out.println(persons.get(i).getFirstName());
-        }
+        System.out.println(tiny.getHeight());
 
     }
 
